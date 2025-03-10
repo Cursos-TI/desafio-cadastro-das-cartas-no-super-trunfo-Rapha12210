55f500db4e6e0db9b4de099dd430ca78094563cd
Rapha12210 <rapha122100@gmail.com>
2025-03-10 21:41:53 +0000

    inseri as variáveis

--- Super_Trunfo.c
+++ Super_Trunfo.c
@@ -1,1 +1,14 @@
-#e
+#include <stdio.h>
+
+int main() {
+
+    int populacao = 5, pontos_turisticos = 5;
+    float area = 10, PIB = 10;
+    char nome[50], estado[10], codigo[20];
+
+    printf(" CADASTRO DE CARTA 01:\n");
+
+    printf("Digite o uma letra que represente o estado:\n");
+    scanf("")
+
+}
